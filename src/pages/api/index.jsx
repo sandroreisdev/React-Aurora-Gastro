8338d83dc8fe91f54818a366dbcb5473c117391d
sandroreisdev <sandroreisdev@gmail.com>
2025-11-07 11:33:50 -0300

    Add Modal Selected Users

--- src/pages/api/index.jsx
+++ src/pages/api/index.jsx
@@ -6,23 +6,31 @@ import { ModalApi } from '../modalApi'
 
 export const Api = () => {
 
-    const [users, setUsers] = useState([])
-    const [abrirModal, setAbrirModal] = useState(false)
+    const [users, setUsers] = useState([]) //variáveis usadas para aurmazenar ou atribuir o estado do usuário
+
+    const [abrirModal, setAbrirModal] = useState(false) //variáveis usadoas para abrir o modal e atribuir um estado para o modal
+    const [selectedUser, setSelectedUser] = useState(null) //variáveis usadas para selecionar um usuário ou atribuir um valor quando o usuário for selecionado
 
     useEffect(() => {
-        axios.get('https://rickandmortyapi.com/api/character')
-            .then((data) => {
-                console.log(data.data.results)
-                setUsers(data.data.results)
+        axios.get('https://rickandmortyapi.com/api/character')// axios biblioteca utilizada para facilitar a integração com uma Api (get pega os dados do endpoint)
+            .then((data) => { //.then caso positivo retorna os dados da Api
+                console.log(data.data.results) // mostra só o results do array da Api
+                setUsers(data.data.results) // altera o estado do array da api (data) que antes estava vazio exemplo: useState([])
             })
-            .catch(() => {
+            .catch(() => { // caso negativo retorna...
 
             })
     }, [])
 
-    const openModal = () => {
+    const openModal = (personagem) => {//
+        setSelectedUser(personagem)
         setAbrirModal(true)
     }
+
+    const closeModal = () => {
+        setAbrirModal(false)
+    }
+
 
     return (
         <>
@@ -33,14 +41,14 @@ export const Api = () => {
                         <div className="card-grid">
                             <h2>{user.name}</h2>
                             <img src={user.image} alt="{user.name}" />
-                            <button onClick={() => openModal()}>Ver mais</button>
+                            <button onClick={() => openModal(user)}>Ver mais</button>
                         </div>
                     ))}
                 </div>
 
             </div>
 
-            <ModalApi open={abrirModal} close={false}/>
+            <ModalApi open={abrirModal} close={closeModal} user={selectedUser}/>
         </>
     )
 }
--- src/pages/modalApi/index.jsx
+++ src/pages/modalApi/index.jsx
@@ -1,11 +1,18 @@
 import { Box, Modal } from "@mui/material"
+import './style.css'
 
-export const ModalApi = ({open, close}) => {
+export const ModalApi = ({open, close, user}) => {
+
+    if(!user) return null
+
     return(
-        <Modal open={open} onclose={close}>
-            <Box>
-                <h2>Modal aberto</h2>
-
+        <Modal open={open} onClose={close}>
+            <Box className="modal-container">
+                <Box className="card-modal">
+                    <h2>{user.name}</h2>
+                    <img src={user.image} alt="" />
+                    <p>{user.species}</p>
+                </Box>                
             </Box>
         </Modal>
     )
